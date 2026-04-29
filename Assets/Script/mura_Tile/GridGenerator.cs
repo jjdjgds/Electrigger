1bda6f074e317c035620687c16cc6c205f2c33c2
ilv6a6y <168617059+jjdjgds@users.noreply.github.com>
2026-04-29 15:13:57 +0900

    a

--- Assets/Script/mura_Tile/GridGenerator.cs
+++ Assets/Script/mura_Tile/GridGenerator.cs
@@ -1,37 +1,33 @@
 using UnityEngine;
-
-
-
-public class GridGenerator : MonoBehaviour 
+public class GridGenerator : MonoBehaviour
 {
     public GameObject tilePrefab;
     public TilescriptObj tilescriptObj;
-    public Vector2 CenterPoint ;
-
+    public Vector2 CenterPoint;
 
     void Start()
     {
-        float offsetX = (TilescriptObj.width - 1) / 2f;
-        float offsetY = (TilescriptObj.height - 1) / 2f;
+        // プレハブのスケールを間隔に使う
+        float tileWidth = tilePrefab.transform.localScale.x;
+        float tileHeight = tilePrefab.transform.localScale.y;
+
+        float offsetX = (TilescriptObj.width - 1) / 2f * tileWidth;
+        float offsetY = (TilescriptObj.height - 1) / 2f * tileHeight;
 
         for (int y = 0; y < TilescriptObj.height; y++)
         {
             for (int x = 0; x < TilescriptObj.width; x++)
             {
                 int index = y * TilescriptObj.width + x;
-
                 if (tilescriptObj.tiles[index])
                 {
                     Vector3 position = new Vector3(
-                        CenterPoint.x + x - offsetX,
-                        CenterPoint.y - y + offsetY,
+                        CenterPoint.x + x * tileWidth - offsetX,
+                        CenterPoint.y - y * tileHeight + offsetY,
                         0);
-
                     Instantiate(tilePrefab, position, Quaternion.identity, transform);
                 }
             }
         }
     }
-
-
 }
